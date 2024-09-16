466f838545d2f73905cf8a5d3edf9dc0aeecc9dd
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-09-16 20:29:06 +0530

    updated ga + release hook

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,15 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-   readonly VITE_APP_USERNAME: string
-   readonly VITE_APP_PASSWORD: string
+    readonly VITE_APP_PASSWORD: string
+    readonly VITE_APP_USERNAME: string
+    readonly VITE_FIREBASE_API_KEY: string
+    readonly VITE_FIREBASE_APP_ID: string
+    readonly VITE_FIREBASE_AUTH_DOMAIN: string
+    readonly VITE_FIREBASE_MEASUREMENT_ID: string
+    readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
 }
 
 interface ImportMeta {
-   readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv
 }
