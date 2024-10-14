12dc2dd13ebf75ad109c7a09e3659bdb7a4583aa
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-10-14 16:27:17 +0530

    integrated og + title metadata

--- nextjs/src/app/downloads/page.tsx
+++ nextjs/src/app/downloads/page.tsx
@@ -1,9 +1,11 @@
 import { GridReleases } from "@/app/downloads/components/grid"
 import { fetchAllGithubReleases } from "@/lib/data/fetch-github-releases"
-import Avatar from "@mui/material/Avatar"
-import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
-import { styled } from "@mui/material/styles"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+   title: "Downloads",
+}
 
 export default async function Page() {
    const releases = await fetchAllGithubReleases()
