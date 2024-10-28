00d74a8de6b825d797bf145a25cbd2ee2f693213
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-10-28 20:16:31 +0530

    Update report-btn.tsx

--- nextjs/src/lib/formbricks/report-btn.tsx
+++ nextjs/src/lib/formbricks/report-btn.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import formbricks from "@formbricks/js";
+import { BugReport } from "@mui/icons-material";
 import { Button, CircularProgress, MenuItem } from "@mui/material";
 import React from "react";
 
@@ -28,12 +29,12 @@ const useAsyncStatus = () => {
 export const FormbricksReportElement = (props : { isButton?: boolean }) => {
     const {isError, isPending, triggerSurveryForm} = useAsyncStatus() 
 
-    let El:React.ReactNode = "Report"
+    let El:React.ReactNode = <BugReport />
     if(isPending) El = <CircularProgress size={20} />
     if(isError) El = "Try again, report"
 
     if(props.isButton)
         return <Button onClick={triggerSurveryForm}>{El}</Button>
 
-    return <MenuItem onClick={triggerSurveryForm}>{El}</MenuItem>
+    return <MenuItem onClick={triggerSurveryForm}>Request for a feature/ Report a bug {El}</MenuItem>
 }
